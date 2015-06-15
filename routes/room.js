9e4034c80a41ddb4026348f7e650c618f01a26ce
Maxime Brazeilles <maxime.brazeilles@gmail.com>
2015-06-15 16:18:28 +0200

    initial commit

---
+++ routes/room.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var redis = require('redis')
+var client = redis.createClient();
+var debug =  require('debug')('cours:roomAction');
+
+
+/* GET home page. */
+router.post('/', function(req, res) {
+  client.incr('room_id', function(err, replies){
+    client.sadd('rooms_list', replies, redis.print)
+  });
+
+  res.redirect('/rooms');
+});
+
+router.get('/', function(req, res) {
+  client.smembers('rooms_list', function(err, replies){
+    res.render('rooms/index', { rooms: replies || [] })
+  });
+});
+
+
+module.exports = router;
